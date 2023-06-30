271e3f1c1ba6b718ada3f2f37a7765a3a60c5d95
Rajshree Umarekar <rajshreeumarekar7@gmail.com>
2023-06-30 17:48:26 +0530

    Added new species:

--- Save-Wildlife/save-wildlife/src/shared/types.js
+++ Save-Wildlife/save-wildlife/src/shared/types.js
@@ -57,27 +57,83 @@ export const TYPES= [
     },
     {
         id: 8,
+        speciesId: 2,
+        name: 'Elephant',
+        image: '/assets/images/herbivores/elephant.jpg',
+        description: 'Elephants are the largest existing land animals.'
+    },
+    {
+        id: 9,
+        speciesId: 2,
+        name: 'Deer',
+        image: '/assets/images/herbivores/deer.jpg',
+        description: 'A deer or true deer is a hoofed ruminant mammal of the family Cervidae.'
+    },
+    {
+        id: 10,
+        speciesId: 2,
+        name: 'Horse',
+        image: '/assets/images/herbivores/horse.jpg',
+        description: 'The horse is a domesticated, one-toed, hoofed mammal. It belongs to the taxonomic family Equidae and is one of two extant subspecies of Equus ferus.'
+    },
+    {
+        id: 11,
+        speciesId: 2,
+        name: 'Giant Panda ',
+        image: '/assets/images/herbivores/panda.jpg',
+        description: 'The giant panda is a bear species endemic to China. It is characterised by its bold black-and-white coat and rotund body.'
+    },
+    {
+        id: 12,
+        speciesId: 3,
+        name: 'Bear',
+        image: '/assets/images/omnivores/bear.jpg',
+        description: 'Bears are mammals that are known to eat fruits, honey, as well as fishes. '
+    },
+    {
+        id: 13,
+        speciesId: 3,
+        name: 'Dog',
+        image: '/assets/images/omnivores/dog.jpg',
+        description: 'The dog is a domesticated descendant of the wolf. Dogs can survive on both meat and plant products.'
+    },
+    {
+        id: 14,
+        speciesId: 3,
+        name: 'Cat',
+        image: '/assets/images/omnivores/cat.jpeg',
+        description: 'The cat is a domestic species of small carnivorous mammal.'
+    },
+    {
+        id: 14,
+        speciesId: 3,
+        name: 'Fox',
+        image: '/assets/images/omnivores/fox.jpg',
+        description: 'Foxes are small to medium-sized, omnivorous mammals belonging to several genera of the family Canidae.'
+    },
+    {
+        id: 15,
         speciesId: 4,
         name: 'Cobra',
         image: '/assets/images/reptiles/cobra.jpg',
         description: 'Cobra is a poisonous snake with hollow fangs fixed to the top jaw at the front of the mouth.'
     },
     {
-        id: 9,
+        id: 16,
         speciesId: 4,
         name: 'Sea Turtle',
         image: '/assets/images/reptiles/sea-turtle.jpg',
         description: 'Sea turtles have streamlined bodies and large flippers that are well-adapted to life in the ocean. '
     },
     {
-        id: 10,
+        id: 17,
         speciesId: 4,
         name: 'Alligator',
         image: '/assets/images/reptiles/alligator.jpg',
         description: 'Alligators are dark green or black, have u-shaped snouts, tiny black spots.'
     },
     {
-        id: 11,
+        id: 18,
         speciesId: 4,
         name: 'Cottonmouths',
         image: '/assets/images/reptiles/cottonmouth.jpg',
